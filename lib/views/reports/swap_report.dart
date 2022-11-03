ec54d866423e3d35b1c969f6831e952e7e32f9c5
Dinesh V <88471929+dineshbalan001@users.noreply.github.com>
2022-11-03 17:55:56 +0530

    Update swap_report.dart

--- lib/views/reports/swap_report.dart
+++ lib/views/reports/swap_report.dart
@@ -18,6 +18,7 @@ class _SwapReportWidgetState extends State<SwapReportWidget> {
       width: 500,
       height: 200,
       child: EasyTable<SwapHistoryModule>(
+          verticalScrollController:ScrollController(),
           EasyTableModel<SwapHistoryModule>(
               rows: widget.SwapHistoryList,
               columns: [
