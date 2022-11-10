b70a3c141b8fcf5d02c143a5ed1a05e9617bee96
Dinesh V <88471929+dineshbalan001@users.noreply.github.com>
2022-11-10 18:09:14 +0530

    Update signup_model.dart

--- lib/models/signup/signup_model.dart
+++ lib/models/signup/signup_model.dart
@@ -52,6 +52,7 @@ class SignupModel extends ChangeNotifier {
             "phonenumber": data["phonenumber"],
             "password": data["password"],
             "DEID": data["DEID"],
+            "UserReferID": data["UserReferID"],
             
           },
           false);
